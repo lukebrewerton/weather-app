0c1560aef4f06a24e9f147c6860cd0fa1f331db5
Luke Brewerton <luke.brewerton@netdimensions.com>
2019-04-25 13:51:52 +0100

    CLI Based

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-const request = require('request');
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode');
 
-const url =
-  'https://api.darksky.net/forecast/e1b8ca647e7f800580664054e6241605/50.82047,-0.13874?units=uk2';
+const location = process.argv[2];
 
-request({ url, json: true }, (error, response) => {
-  console.log(
-    response.body.daily.data[0].summary +
-      ' It is currently ' +
-      response.body.currently.temperature +
-      ' degrees out. There is a ' +
-      response.body.currently.precipProbability +
-      '% chance of rain.'
-  );
-});
+if (location) {
+  geocode(location, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return console.log(error);
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
 
-// Geocoding
-const geocodeUrl =
-  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGItZGV2IiwiYSI6ImNqdXZwaDN2aTAzdWQ0NHFibmppbDBoZmgifQ.7dj_kkebOGAKiTPTlF3xZg&limit=1';
-
-request({ url: geocodeUrl, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
-  console.log(latitude, longitude);
-});
+      console.log(location);
+      console.log(forecastData);
+    });
+  });
+} else {
+  console.log('No Location provided');
+}
